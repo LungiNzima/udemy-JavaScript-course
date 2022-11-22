804d3c6721a3d8df4a5e6a30afb92dbf80223fb1
Lungi Nzima <nzimalungi@gmail.com>
2022-11-22 21:59:15 +0800

    completed the second challenge for basic array operations

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,28 +1,35 @@
 'use strict';
 
-// Test 1
-const calcAverage = (scoreA, scoreB, scoreC) => (scoreA + scoreB + scoreC) / 3;
-let scoreDolphins = calcAverage(44, 23,71);
-let scoreKoalas= calcAverage(65, 54,49);
-console.log(scoreDolphins, scoreKoalas);
-
-const checkWinner = function (avgDolphins, avgKoalas) {
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win! 🏆 (${avgDolphins} vs ${avgKoalas})`);
-  } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log(`Koalas win! 🏆 (${avgKoalas} vs ${avgDolphins})`);
-  } else {
-    console.log(`No team wins.`);
-  }
+const calcTip = function (bill){
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[0])];
+console.log(bills, tips);
 
-checkWinner(scoreDolphins, scoreKoalas);
+// Test 1
+// const calcAverage = (scoreA, scoreB, scoreC) => (scoreA + scoreB + scoreC) / 3;
+// let scoreDolphins = calcAverage(44, 23,71);
+// let scoreKoalas= calcAverage(65, 54,49);
+// console.log(scoreDolphins, scoreKoalas);
 
-// Test 2
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win! 🏆 (${avgDolphins} vs ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win! 🏆 (${avgKoalas} vs ${avgDolphins})`);
+//   } else {
+//     console.log(`No team wins.`);
+//   }
+// }
 
-scoreDolphins = calcAverage(85, 54, 41);
-scoreKoalas= calcAverage(23, 34, 27);
-checkWinner(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// // Test 2
+
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas= calcAverage(23, 34, 27);
+// checkWinner(scoreDolphins, scoreKoalas);
 // let hasDriversLicense = false;
 // const passTest = true;
 
@@ -110,3 +117,55 @@ console.log(yearsUntilRetirement(1993,'Lungi')); */
 // }
 
 // console.log(yearsUntilRetirement(1993, 'Lungi'));
+
+/* const friends = ['Michael', 'Steven', 'Peter'];
+
+//add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+console.log(friends);
+friends.shift(); // first
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+} */
+
+// const years = new Array(1991, 1984, 2000, 2020);
+// console.log(years);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// const firstName = 'Lungi';
+// const lungi = [firstName, 'Nzima', 2020 - 1993, 'teacher, friends'];
+// console.log(lungi);
+
+// // Exercise
+
+// const calcAge = function (birthYear) {
+//   return 2022 - birthYear;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+// console.log(ages);
