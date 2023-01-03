65ed5da163271da28abf99d010f379123a47b890
Lungi Nzima <nzimalungi@gmail.com>
2023-01-03 20:09:24 +0800

    completed challenge 4 on object methods in JS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,47 @@
 'use strict';
 
-const calcTip = function (bill){
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const mark = {
+  fullName: 'Mark Miller',
+  mass: '78',
+  height: '1.69',
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: '92',
+  height: '1.95',
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
 }
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[0])];
-console.log(bills, tips);
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if(mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi})
+  is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi})
+    is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
+
+// const calcTip = function (bill){
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[0])];
+// console.log(bills, tips);
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 // Test 1
 // const calcAverage = (scoreA, scoreB, scoreC) => (scoreA + scoreB + scoreC) / 3;
@@ -169,3 +205,59 @@ if (friends.includes('Steven')) {
 
 // const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 // console.log(ages);
+
+
+/* const lungi = {
+  firstName: 'Lungi',
+  lastName: 'Nzima',
+  age: 2023 - 1993,
+  job: 'teacher',
+  friends: ['Michael', 'John', 'Steve']
+};
+console.log(lungi); */
+
+/* console.log(lungi.lastName);
+console.log(lungi['lastName']);
+
+const nameKey = 'Name';
+console.log(lungi['first' + nameKey]);
+console.log(lungi['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Lungi? Choose between firstName, lastName, age, job, and friends');
+
+if (lungi[interestedIn]) {
+  console.log(lungi[interestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+console.log(`${lungi.firstName} has ${lungi.friends.length}, and her best friend is called ${lungi.friends[0]}`); */
+
+// const lungi = {
+//   firstName: 'Lungi',
+//   lastName: 'Nzima',
+//   birthYear: 1993,
+//   job: 'teacher',
+//   friends: ['Michael', 'John', 'Steve'],
+//   hasDriversLicense: true,
+
+//   // calcAge: function () {
+//   //   console.log(this);
+//   //   return 2023 - this.birthYear;
+//   // }
+
+//   calcAge: function() {
+//     this.age = 2023 - this.birthYear;
+//     return this.age;
+//   },
+
+//   getSummary: function () {
+//     return `${this.firstName} is a ${this.calcAge()}-year old ${lungi.job},
+//     and she has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`
+//   }
+// };
+
+// console.log(lungi.calcAge());
+// console.log(lungi.age);
+
+// console.log(lungi.getSummary());
